1db6298123d8fc3dc204aafde9ce7d63916c9fea
joaoscam <157593287+joaoscam@users.noreply.github.com>
2025-09-21 17:56:13 -0300

    chore: update endpoint name

--- src/main/java/com/apisql/ApiSQL/controller/ViagemController.java
+++ src/main/java/com/apisql/ApiSQL/controller/ViagemController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/viagens")
+@RequestMapping("/viagens")
 @Tag(name = "Viagens", description = "API para gerenciamento de viagens")
 public class ViagemController {
 
