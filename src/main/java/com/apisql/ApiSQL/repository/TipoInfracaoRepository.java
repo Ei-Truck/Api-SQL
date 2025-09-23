781469ef319252fbc3293ffea4e544fd5e68a7b5
joaoscam <joao.camargo@germinare.org.br>
2025-09-23 07:37:29 -0300

    feat(entity): new entity JPA infingement type

---
+++ src/main/java/com/apisql/ApiSQL/repository/TipoInfracaoRepository.java
@@ -0,0 +1,7 @@
+package com.apisql.ApiSQL.repository;
+
+import com.apisql.ApiSQL.model.TipoInfracao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoInfracaoRepository extends JpaRepository<TipoInfracao, Integer> {
+}
