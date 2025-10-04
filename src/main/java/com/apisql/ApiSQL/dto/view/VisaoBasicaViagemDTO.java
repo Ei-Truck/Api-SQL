d7f4a76d8f3940e176782a6cc79a4fa2a55113fe
DanielTogSevero <danieldonepios@gmail.com>
2025-10-04 17:52:50 -0300

    feat(view-visao-basica)new-consultation-for-visao-basica-viagem-view

---
+++ src/main/java/com/apisql/ApiSQL/dto/view/VisaoBasicaViagemDTO.java
@@ -0,0 +1,145 @@
+package com.apisql.ApiSQL.dto.view;
+
+import java.time.LocalDateTime;
+
+public class VisaoBasicaViagemDTO {
+
+    private String placaCaminhao;
+    private LocalDateTime dataInicioViagem;
+    private LocalDateTime dataFimViagem;
+    private String segmento;
+    private String nomeMotorista;
+    private String riscoMotorista;
+    private Integer idMidiaConcatenada;
+    private String urlMidiaConcatenada;
+    private Integer idViagem;
+    private Integer idSegmento;
+    private Integer idMotorista;
+    private Integer idTipoGravidade;
+    private Integer idTipoRisco;
+    private Integer idInfracao;
+    private Integer idCaminhao;
+
+    public VisaoBasicaViagemDTO() {
+    }
+
+    public String getPlacaCaminhao() {
+        return placaCaminhao;
+    }
+
+    public void setPlacaCaminhao(String placaCaminhao) {
+        this.placaCaminhao = placaCaminhao;
+    }
+
+    public LocalDateTime getDataInicioViagem() {
+        return dataInicioViagem;
+    }
+
+    public void setDataInicioViagem(LocalDateTime dataInicioViagem) {
+        this.dataInicioViagem = dataInicioViagem;
+    }
+
+    public LocalDateTime getDataFimViagem() {
+        return dataFimViagem;
+    }
+
+    public void setDataFimViagem(LocalDateTime dataFimViagem) {
+        this.dataFimViagem = dataFimViagem;
+    }
+
+    public String getSegmento() {
+        return segmento;
+    }
+
+    public void setSegmento(String segmento) {
+        this.segmento = segmento;
+    }
+
+    public String getNomeMotorista() {
+        return nomeMotorista;
+    }
+
+    public void setNomeMotorista(String nomeMotorista) {
+        this.nomeMotorista = nomeMotorista;
+    }
+
+    public String getRiscoMotorista() {
+        return riscoMotorista;
+    }
+
+    public void setRiscoMotorista(String riscoMotorista) {
+        this.riscoMotorista = riscoMotorista;
+    }
+
+    public Integer getIdMidiaConcatenada() {
+        return idMidiaConcatenada;
+    }
+
+    public void setIdMidiaConcatenada(Integer idMidiaConcatenada) {
+        this.idMidiaConcatenada = idMidiaConcatenada;
+    }
+
+    public String getUrlMidiaConcatenada() {
+        return urlMidiaConcatenada;
+    }
+
+    public void setUrlMidiaConcatenada(String urlMidiaConcatenada) {
+        this.urlMidiaConcatenada = urlMidiaConcatenada;
+    }
+
+    public Integer getIdViagem() {
+        return idViagem;
+    }
+
+    public void setIdViagem(Integer idViagem) {
+        this.idViagem = idViagem;
+    }
+
+    public Integer getIdSegmento() {
+        return idSegmento;
+    }
+
+    public void setIdSegmento(Integer idSegmento) {
+        this.idSegmento = idSegmento;
+    }
+
+    public Integer getIdMotorista() {
+        return idMotorista;
+    }
+
+    public void setIdMotorista(Integer idMotorista) {
+        this.idMotorista = idMotorista;
+    }
+
+    public Integer getIdTipoGravidade() {
+        return idTipoGravidade;
+    }
+
+    public void setIdTipoGravidade(Integer idTipoGravidade) {
+        this.idTipoGravidade = idTipoGravidade;
+    }
+
+    public Integer getIdTipoRisco() {
+        return idTipoRisco;
+    }
+
+    public void setIdTipoRisco(Integer idTipoRisco) {
+        this.idTipoRisco = idTipoRisco;
+    }
+
+    public Integer getIdInfracao() {
+        return idInfracao;
+    }
+
+    public void setIdInfracao(Integer idInfracao) {
+        this.idInfracao = idInfracao;
+    }
+
+    public Integer getIdCaminhao() {
+        return idCaminhao;
+    }
+
+    public void setIdCaminhao(Integer idCaminhao) {
+        this.idCaminhao = idCaminhao;
+    }
+}
