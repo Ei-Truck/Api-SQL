64547e3bd8db41764dfd73d25151ca3047d4b803
joaoscam <joao.camargo@germinare.org.br>
2025-10-31 16:49:17 -0300

    fix(view) fields values

--- src/main/java/com/apisql/ApiSQL/repository/view/QuantidadeInfracaoTipoGravidadeRepository.java
+++ src/main/java/com/apisql/ApiSQL/repository/view/QuantidadeInfracaoTipoGravidadeRepository.java
@@ -12,7 +12,7 @@ public class QuantidadeInfracaoTipoGravidadeRepository {
     private EntityManager em;
 
     public List<Object[]> buscarQntdInfracoes(Integer idViagem) {
-        return em.createNativeQuery("SELECT tipo_leve, tipo_media, tipo_grave, tipo_gravissima FROM vw_quantidade_infracao_tipo_gravidade WHERE id_viagem = ?1")
+        return em.createNativeQuery("SELECT id_viagem, id_motorista, id_unidade, id_localidade, tipo_leve, tipo_media, tipo_grave, tipo_gravissima FROM vw_quantidade_infracao_tipo_gravidade WHERE id_viagem = ?1")
                 .setParameter(1, idViagem)
                 .getResultList();
     }
